4ddae7332965d5645b0b2c0df1d25998a40f39ce
SadeghSeyfi <weblogsadegh@gmail.com>
2023-07-25 16:06:17 +0330

    Add project files.

---
+++ Gold_Quiz/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gold_Quiz.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
