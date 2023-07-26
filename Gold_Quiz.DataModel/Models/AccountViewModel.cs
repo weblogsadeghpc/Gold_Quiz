b61e1eb4c5119620a8d8560f6d2131fb98e5e4c0
SadeghSeyfi <weblogsadegh@gmail.com>
2023-07-26 11:59:53 +0330

    Create Login Page , add font awsomes,change background and add new icon

---
+++ Gold_Quiz.DataModel/Models/AccountViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gold_Quiz.DataModel.Models
+{
+    public class LoginViewModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "شماره موبایل وارد نشده است.")] //halate ejbari
+        [StringLength(11, ErrorMessage = "شماره موبایل 11 رقمی می باشد.")] // tedad horof
+        [RegularExpression(@"^[^\\/:*;\.\)\(]+$", ErrorMessage = "کاراکترهای غیر مجاز وارد شده است.")] // format khas shomare mobile
+        public string UserName { get; set; } //Phone Number
+        [Required(AllowEmptyStrings = false, ErrorMessage = "پسورد وارد نشده است.")] //halate ejbari
+        public string Password { get; set; }
+    }
+}
