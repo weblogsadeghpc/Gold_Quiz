939045a9a13289c50b7327625e80f0aed2325ff4
SadeghSeyfi <weblogsadegh@gmail.com>
2023-08-01 16:28:30 +0330

    ijad bakhshe modaresin  - mdel ham bayad az tarighe TeacherManagementController ersal konim tazrighe vabastegi konim model = IEnumerable<CenterUsers>

---
+++ Gold_Quiz.DataModel/Models/TeacherViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gold_Quiz.DataModel.Models
+{
+    public class TeacherViewModel
+    {
+        [Display(Name = "نام")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "نام کوچک وارد نشده است .")]
+        [StringLength(maximumLength: 60, ErrorMessage = "نام حداکثر 60 کاراکتر میباشد .")]
+        public string FirstName { get; set; }
+        [Display(Name = "نام خانوادگی")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "نام خانوادگی وارد نشده است .")]
+        [StringLength(maximumLength: 250, ErrorMessage = "نام خانوادگی حداکثر 250 کاراکتر میباشد .")]
+        public string Family { get; set; }
+        [Display(Name = "شماره تماس")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "شماره موبایل وارد نشده است.")]
+        [StringLength(11, ErrorMessage = "شماره موبایل 11 رقمی می باشد .")]
+        [RegularExpression(@"^[^\\/:*;\.\)\(]+$", ErrorMessage = "کاراکترهای غیر مجاز وارد شده است.")]
+        public string UserName { get; set; }
+
+        [Display(Name = "انتخاب درس")] // moalem che darsie
+        public int CourseID { get; set; }
+    }
+}
---
+++ Gold_Quiz/Areas/AdminPanel/Controllers/TeacherManagementController.cs
@@ -0,0 +1,114 @@
+﻿using AutoMapper;
+using Gold_Quiz.DataModel.Entities;
+using Gold_Quiz.DataModel.Models;
+using Gold_Quiz.DataModel.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace Gold_Quiz.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    [Authorize(Roles = "Admin")]
+    public class TeacherManagementController : Controller
+    {
+        private readonly IUnitOfWork _context;
+        private readonly UserManager<ApplicationUsers> _userManager;
+        private readonly IMapper _mapper;
+
+        public TeacherManagementController(IUnitOfWork context, UserManager<ApplicationUsers> userManager, IMapper mapper)
+        {
+            _context = context;
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+        public IActionResult Index()
+        {
+            var model = _context.centerUsersUW.
+                Get(c => c.CenterAdminID == _userManager.GetUserId(HttpContext.User) && c.Users_ST.UserType == 2, "Users_ST");
+            // hamin admin id ke toye system hast ro bia neshon bede users haye khodesh ro neshon bede
+            // dar centerusers be name moalem dastresi nadaim join ham bayad konim 
+            // listi  az center users vali ba shart
+            // && List moalemin bedone && feghat list kole karbaran ast 2 = moalem , 1 = Admin ..
+            return View(model);
+        }
+
+        [HttpGet] // action method az type get
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(TeacherViewModel model)
+        {
+            if (ModelState.IsValid) // yani hame etelaat be dorosti vared shode bood
+            {
+                using (var tr = _context.BaseTransaction())
+                {
+                    // نقش عملیات را دارد
+                    //Register
+                    try
+                    {
+                        var user = await _userManager.FindByNameAsync(model.UserName);
+                        if (user != null)
+                        {
+                            // user peyda shod
+                            ModelState.AddModelError("UserName", "شماره تماس شما در سیستم ثبت شده است .");// enteha neshan bede hamoon akhar ke ConfirmPassword neveshtim 
+                            return View(model);
+                        }
+
+                        var mapUser = _mapper.Map<ApplicationUsers>(model);
+                        mapUser.PhoneNumber = model.UserName; // phone number hamoon username ast
+                        mapUser.UserType = 2;//Teacher
+                        mapUser.IsActive = 1;// Active
+
+                        //amaliate sabte nam
+                        // 1 - ثبت کاربر در دیتابیس
+                        IdentityResult result = await _userManager.CreateAsync(mapUser, "123456");// karbar dar in khat sabte nam mishe // password sade gharar midim bar aval 
+                        if (result.Succeeded)
+                        {
+                            // 2 - ثبت نقش برای کاربر
+                            await _userManager.AddToRoleAsync(mapUser, "Teacher");// be karbar mapuser yek role bede // Role admin be user dadim 
+
+                            // 3 - ثبت اطلاعات مدرس در مرکز
+                            CenterUsers CU = new CenterUsers
+                            {
+                                CenterAdminID = _userManager.GetUserId(HttpContext.User),// hamin usr ke dare etelaat ro submit mikone user admin ast 
+                                CenterID = 0,
+                                CenterUserID = mapUser.Id // id modares ya daneshamooz
+                            };
+                            _context.centerUsersUW.Create(CU);
+                            tr.commit();
+                            _context.Save();
+
+                            // estefade az transaction ha baraye inke ya hamash anjam beshe ya aslan anjam nashe chon mesalan vasatesh internet ghat shod moshke nakhore mesalan Role sakhte nashe az transaction estefade mikonim ke mesle yek tarakonesh kar mikone
+
+                            return RedirectToAction("SuccesfullyRegister");
+                            // 3 ja etelaat sabt mishe : 
+                            //1- Role
+                            //2- User
+                            //3- Centers
+                        }
+                        return RedirectToAction("Error", "Home");
+                    }
+                    catch (Exception)
+                    {
+                        tr.RollBack(); // hich kari anjam nade
+                        return RedirectToAction("Error", "Home"); // agar error didi boro be controller => home action => error 
+
+                    }
+                }
+            }
+            else
+            {
+                //namayesh khataha
+                return View(model);
+            }
+        }
+    }
+}
